2e9652a121a7238792ffec91f9d28e44885bc030
Leevesh Kamboj <leeveshk12@gmail.com>
2022-01-10 14:43:14 +0530

    added get response route

--- resolvers/response.js
+++ resolvers/response.js
@@ -2,58 +2,72 @@ const Forms = require("../models/forms");
 const config = require("../config");
 
 const responsePostResolver = async (req, res) => {
-    var result = null;
-    if (!req.params.formID)
-      return res.status(401).json({ success: false, error: "ID not provided" });
-    try {
-      result = await Forms.findById(req.params.formID);
-    } catch {
-      return res.status(404).json({ success: false, error: "Form not found" });
-    }
-    if (
-      !req.body.responses ||
-      req.body.responses.length !== result.questions.length
-    ) {
-      return res
-        .status(401)
-        .json({ success: false, error: "Responses not provided" });
-    }
-    if (result) {
-      const responses = req.body.responses.filter((val, i) => {
-        if (
-          val !== null &&
-          config.quetionTypesWithOptions.includes(result.questions[i].type)
-        ) {
-          return !isNaN(val) && val < result.questions[i].options.length;
-        }
-        return true;
-      });
-      if (responses.length !== result.questions.length) {
-        return res
-          .status(401)
-          .json({ success: false, error: "Responses not valid" });
-      }
-      result.responses.push(
-        responses.map((val, i) => {
-          if (
-            val !== null &&
-            config.quetionTypesWithOptions.includes(result.questions[i].type)
-          )
-            return parseInt(val);
-          else return val;
-        })
-      );
-      result
-        .save()
-        .then(() => {
-          return res.status(200).json({ success: true });
-        })
-        .catch(() => {
-          return res
-            .status(500)
-            .json({ success: false, error: "Internal server Error" });
-        });
-    }
-  };
+  if (!req.params.formID)
+    return res.status(401).json({ success: false, error: "ID not provided" });
+  try {
+    result = await Forms.findById(req.params.formID);
+  } catch {
+    return res.status(404).json({ success: false, error: "Form not found" });
+  }
+  if (
+    !req.body.responses ||
+    req.body.responses.length !== result.questions.length
+  ) {
+    return res
+      .status(401)
+      .json({ success: false, error: "Responses not provided" });
+  }
 
-  module.exports = { responsePostResolver };
+  const responses = req.body.responses.filter((val, i) => {
+    if (
+      val !== null &&
+      config.quetionTypesWithOptions.includes(result.questions[i].type)
+    ) {
+      return !isNaN(val) && val < result.questions[i].options.length;
+    }
+    return true;
+  });
+  if (responses.length !== result.questions.length) {
+    return res
+      .status(401)
+      .json({ success: false, error: "Responses not valid" });
+  }
+  result.responses.push(
+    responses.map((val, i) => {
+      if (
+        val !== null &&
+        config.quetionTypesWithOptions.includes(result.questions[i].type)
+      )
+        return parseInt(val);
+      else return val;
+    })
+  );
+  result
+    .save()
+    .then(() => {
+      return res.status(200).json({ success: true });
+    })
+    .catch(() => {
+      return res
+        .status(500)
+        .json({ success: false, error: "Internal server Error" });
+    });
+};
+
+const responseGetResolver = async (req, res) => {
+  if (!req.params.formID)
+    return res.status(401).json({ success: false, error: "ID not provided" });
+  try {
+    result = await Forms.findById(req.params.formID).populate("created_by");
+  } catch {
+    return res.status(404).json({ success: false, error: "Form not found" });
+  }
+  if (result.created_by.username !== req.user.username) {
+    return res
+      .status(401)
+      .json({ success: false, error: "Form is not created by you." });
+  }
+  return res.status(200).json({ success: true, responses: result.responses });
+};
+
+module.exports = { responsePostResolver, responseGetResolver };
--- routes/form.js
+++ routes/form.js
@@ -5,7 +5,10 @@ const {
   formGetResolver,
   formsGetResolver,
 } = require("../resolvers/form");
-const { responsePostResolver } = require("../resolvers/response");
+const {
+  responsePostResolver,
+  responseGetResolver,
+} = require("../resolvers/response");
 const { authentication_middleware } = require("../middlewares");
 
 const router = express.Router();
@@ -13,6 +16,7 @@ const router = express.Router();
 router.post("/", authentication_middleware, formPostResolver);
 router.get("/", authentication_middleware, formsGetResolver);
 router.get("/:formID", formGetResolver);
+router.get("/:formID/response", authentication_middleware, responseGetResolver);
 router.post("/:formID/response", responsePostResolver);
 
 module.exports = router;
