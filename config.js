16ad886482b6e94335628fcd5f11d339eced5275
Leevesh Kamboj <leeveshk12@gmail.com>
2022-01-11 12:07:15 +0530

    added mcq type

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const config = {
   port: process.env.PORT || 4000,
   dbUrl: process.env.DB_URL || null,
   tokenKey: process.env.TOKEN_KEY || "test",
-  quetionTypes: [0, 1, 2],
-  quetionTypesWithOptions: [2],
+  quetionTypes: [0, 1, 2, 3],
+  quetionTypesWithOptions: [2, 3],
 };
 
 module.exports = config;
